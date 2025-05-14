f2670f7f02d7ed3f6504fe5e2f63b13091b75989
Cristian Camilo <crca9623@gmail.com>
2025-05-13 21:11:07 -0500

    Callback Hell en el proyecto advance

--- advance/src/concepts/callbacks.js
+++ advance/src/concepts/callbacks.js
@@ -6,17 +6,28 @@ import { heroes } from "../data/heroes";
  */
 export const callbacksComponent = ( element ) => {
 
-    const id = '5d86371fd55e2e2a30fe1ccb13';
-    findHero( id, (error, hero) => {
+    const id1 = '5d86371fd55e2e2a30fe1ccb1';
+    const id2 = '5d86371fd55e2e2a30fe1ccb2';
+    findHero( id1, (error, hero1) => {
 
         if (error) {
             element.innerHTML = error;
             return
         }
 
-        element.innerHTML = hero.name;
+        findHero( id2, (error, hero2) => {
+
+            if (error) {
+                element.innerHTML = error;
+                return
+            }
+    
+            element.innerHTML = `${ hero1.name } / ${ hero2.name }`;
+            
+        } )
         
     } )
+
     
 }
 
