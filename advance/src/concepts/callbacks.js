61570ab68884174b5f9b16c0fe755bd2b013f934
Cristian Camilo <crca9623@gmail.com>
2025-05-13 21:07:13 -0500

    Manejo de errores en callbacks en el proyecto advance

--- advance/src/concepts/callbacks.js
+++ advance/src/concepts/callbacks.js
@@ -6,8 +6,13 @@ import { heroes } from "../data/heroes";
  */
 export const callbacksComponent = ( element ) => {
 
-    const id = '5d86371fd55e2e2a30fe1ccb1';
-    findHero( id, (hero) => {
+    const id = '5d86371fd55e2e2a30fe1ccb13';
+    findHero( id, (error, hero) => {
+
+        if (error) {
+            element.innerHTML = error;
+            return
+        }
 
         element.innerHTML = hero.name;
         
@@ -18,11 +23,16 @@ export const callbacksComponent = ( element ) => {
 /**
  * 
  * @param {String} id 
- * @param { (hero: Object)=> void} callback 
+ * @param { (error: String|Null, hero: Object)=> void} callback 
  */
 const findHero = ( id, callback ) => {
 
-    const hero = heroes.find( hero => hero.id === id)
+    const hero = heroes.find( hero => hero.id === id);
 
-    callback( hero )
+    if ( !hero ) {
+        callback(`Hero with id ${ id } not found`);
+        return;
+    }
+
+    callback( null, hero )
 }
